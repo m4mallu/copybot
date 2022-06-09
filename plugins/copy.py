cd6f012d01135c9cc0c80b5b717a2c6fb5f0c0d5
Renjith Mangal <65238086+m4mallu@users.noreply.github.com>
2022-06-09 13:35:08 +0530

    Update copy.py

--- plugins/copy.py
+++ plugins/copy.py
@@ -16,7 +16,8 @@ async def forward(bot, update):
         await bot.copy_message(
             chat_id=Config.CHANNEL_ID,
             from_chat_id=update.chat.id,
-            message_id=update.message_id
+            message_id=update.message_id,
+            caption=update.caption
         )
     except FloodWait as e:
         time.sleep(e.x)
